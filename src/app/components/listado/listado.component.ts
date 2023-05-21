37a580fd5c75bacbf56873363c2471d0e432f25c
Rene Arrieta <renearrieta8905@gmail.com>
2023-05-21 14:27:03 -0500

    Agregado listado

--- src/app/components/listado/listado.component.ts
+++ src/app/components/listado/listado.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+interface Pregunta {
+  texto: string;
+  respuesta: string;
+  esconder: boolean
+}
 @Component({
   selector: 'app-listado',
   templateUrl: './listado.component.html',
   styleUrls: ['./listado.component.css']
 })
 export class ListadoComponent {
+
+  preguntas: Pregunta[];
+
+
+  constructor() {
+    this.preguntas=[
+      {
+        texto: "¿Cuál es tu nombre?",
+        respuesta: "Mi nombre es René",
+        esconder: true
+      },
+      {
+        texto: "¿Cuál es tu color favorito?",
+        respuesta: "El azul",
+        esconder: true
+      },
+      {
+        texto: "¿Cuál es tu super héroe favorito?",
+        respuesta: "Batman",
+        esconder: true
+      },
+    ];
+  }
+  ngOnInit(){
+
+  }
 
 }
